3b83eacf06a711966ee99078f8dbf18d449432fe
tonysuu <tonysu1122@hotmail.com>
2015-03-07 20:47:22 -0500

    added scanning funciton(not working)

--- app/src/main/java/wakeup/timmui/me/wakeup/MainActivity.java
+++ app/src/main/java/wakeup/timmui/me/wakeup/MainActivity.java
@@ -1,11 +1,21 @@
 package wakeup.timmui.me.wakeup;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.thalmic.myo.AbstractDeviceListener;
+import com.thalmic.myo.Arm;
+import com.thalmic.myo.DeviceListener;
 import com.thalmic.myo.Hub;
+import com.thalmic.myo.Myo;
+import com.thalmic.myo.Pose;
+import com.thalmic.myo.Quaternion;
+import com.thalmic.myo.XDirection;
+import com.thalmic.myo.scanner.ScanActivity;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,6 +24,7 @@ import java.util.TimerTask;
 public class MainActivity extends ActionBarActivity {
 
     private final int DELAY = 5000;
+    private DeviceListener mListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +32,32 @@ public class MainActivity extends ActionBarActivity {
 
 
         Hub hub = Hub.getInstance();
+        if (!hub.init(this)) {
+            //Log.e(TAG, "Could not initialize the Hub.");
+            finish();
+            return;
+        }
+       /* mListener = new AbstractDeviceListener() {
+            @Override
+            public void onConnect(Myo myo, long timestamp) {
+                Toast.makeText(mContext, "Myo Connected!", Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onDisconnect(Myo myo, long timestamp) {
+                Toast.makeText(mContext, "Myo Disconnected!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPose(Myo myo, long timestamp, Pose pose) {
+                Toast.makeText(mContext, "Pose: " + pose, Toast.LENGTH_SHORT).show();
+
+                //TODO: Do something awesome.
+            }
+        };*/
+        //hub.addListener(mListener);
+
+        //Instantiates a new timer object
         Timer timer = new Timer();
         final Runnable runnable = new Runnable(){
             public void run(){
@@ -42,21 +78,24 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (R.id.action_scan == id) {
+            onScanActionSelected();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    private void onScanActionSelected() {
+        // Launch the ScanActivity to scan for Myos to connect to.
+        Intent intent = new Intent(this, ScanActivity.class);
+        startActivity(intent);
     }
 }
